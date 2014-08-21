072c1f8c6fc1d613fead95bbf0315acb95a3220d
alinulms <alinulms@gmail.com>
2014-08-21 22:56:18 +0200

    added search

---
+++ Website/Presentation/Controls/SearchControl.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SearchControl.ascx.cs" Inherits="Website.Presentation.Controls.SearchControl" %>
+<div>
+    <asp:TextBox ID="searchTxt" runat="server"/>
+    <asp:Button runat="server" ID="btnSearch" OnClick="SearchDiscussion" Text="Search"/>
+</div>
---
+++ Website/Presentation/Controls/SearchControl.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website.Presentation.Controls
+{
+  public partial class SearchControl : System.Web.UI.UserControl
+  {
+    protected void SearchDiscussion(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
