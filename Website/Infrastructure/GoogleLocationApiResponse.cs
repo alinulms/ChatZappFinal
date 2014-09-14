d2313cd537000ce996a3c2ee4f8ebfb190973b65
alinulms <alinulms@gmail.com>
2014-09-14 17:26:10 +0200

    added files

---
+++ Website/Infrastructure/GoogleLocationApiResponse.cs
@@ -0,0 +1,54 @@
+﻿namespace Website.Infrastructure
+{
+  using System.Collections.Generic;
+
+  public class Location
+  {
+    public double lat { get; set; }
+    public double lng { get; set; }
+  }
+
+  public class Northeast
+  {
+    public double lat { get; set; }
+    public double lng { get; set; }
+  }
+
+  public class Southwest
+  {
+    public double lat { get; set; }
+    public double lng { get; set; }
+  }
+
+  public class Viewport
+  {
+    public Northeast northeast { get; set; }
+    public Southwest southwest { get; set; }
+  }
+
+  public class Geometry
+  {
+    public Location location { get; set; }
+    public Viewport viewport { get; set; }
+  }
+
+  public class Result
+  {
+    public Geometry geometry { get; set; }
+    public string icon { get; set; }
+    public string id { get; set; }
+    public string name { get; set; }
+    public string place_id { get; set; }
+    public string reference { get; set; }
+    public string scope { get; set; }
+    public List<string> types { get; set; }
+    public string vicinity { get; set; }
+  }
+
+  public class GoogleLocationApiResponse
+  {
+    public List<object> html_attributions { get; set; }
+    public List<Result> results { get; set; }
+    public string status { get; set; }
+  }
+}
---
+++ Website/Infrastructure/GooglePlacesService.cs
@@ -0,0 +1,69 @@
+﻿namespace Website.Infrastructure
+{
+  using System;
+  using System.Globalization;
+  using System.Linq;
+  using System.Net;
+  using System.Runtime.Serialization.Json;
+  using System.Text;
+
+  public class GooglePlacesService
+  {
+    private string _latitude;
+    private string _longitude;
+    private const string GooglePlacesServiceUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+    public GooglePlacesService(string latitude, string longitude)
+    {
+      _latitude = latitude;
+      _longitude = longitude;
+    }
+
+    public string GetNearbyPlace()
+    {
+      string urlRequest = BuildGooglePlacesUrl();
+      GoogleLocationApiResponse response = MakeRequest(urlRequest);
+      return response.results.FirstOrDefault().name;
+    }
+
+    private string BuildGooglePlacesUrl()
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.Append(GooglePlacesServiceUrl);
+      sb.Append("location=");
+      sb.Append(_latitude.ToString(CultureInfo.InvariantCulture).Replace(',', '.'));
+      sb.Append(",");
+      sb.Append(_longitude.ToString(CultureInfo.InvariantCulture).Replace(',', '.'));
+      sb.Append("&radius=1");
+      sb.Append("&key=");
+      sb.Append(Constants.GoogleApiKey);
+      return sb.ToString();
+    }
+
+    public static GoogleLocationApiResponse MakeRequest(string requestUrl)
+    {
+      try
+      {
+        HttpWebRequest request = WebRequest.Create(requestUrl) as HttpWebRequest;
+        using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+        {
+          if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception(String.Format(
+            "Server error (HTTP {0}: {1}).",
+            response.StatusCode,
+            response.StatusDescription));
+          DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(GoogleLocationApiResponse));
+          object objResponse = jsonSerializer.ReadObject(response.GetResponseStream());
+          GoogleLocationApiResponse jsonGoogleLocationApiResponse
+          = objResponse as GoogleLocationApiResponse;
+          return jsonGoogleLocationApiResponse;
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        return null;
+      }
+    }
+  }
+}
