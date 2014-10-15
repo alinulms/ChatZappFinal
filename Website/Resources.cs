22e9c57fef3635c90f315895d087b231f6b8bc27
alinulms <alinulms@gmail.com>
2014-10-15 20:25:54 +0200

    added resources

---
+++ Website/Resources.cs
@@ -0,0 +1,60 @@
+﻿namespace Website
+{
+  public class Resources
+  {
+    public struct C
+    {
+      public const string CreatedBy = "Created on :";
+      public const string ChangeRadius = "Change chat radius";
+    }
+
+    public struct D
+    {
+      public const string DefaultScanRadius = "Default scan radius";
+    }
+
+    public struct F
+    {
+      public const string FacebookLogin = "Facebook login";
+    }
+
+    public struct I
+    {
+      public const string IntroText =
+        "Jean shorts tousled chillwave food truck four loko. Chia Blue Bottle yr occupy drinking vinegar next level. Tote bag craft beer forage occupy, hashtag fingerstache whatever. Trust fund Intelligentsia twee butcher small batch. Hella Godard viral Neutra, plaid Pitchfork mixtape gastropub American Apparel pop-up. Church-key Truffaut farm-to-table, squid jean shorts direct trade raw denim cray 3 wolf moon fashion axe synth. Roof party flexitarian biodiesel, master cleanse lo-fi chia dreamcatcher selvage cornhole twee crucifix paleo locavore.";
+    }
+
+    public struct L
+    {
+      public const string ListDiscussions = "List discussions";
+    }
+
+    public struct N
+    {
+      public const string NewDiscussion = "New discussion";
+      public const string Nearby = "Posted nearby : ";
+    }
+
+    public struct P
+    {
+      public const string PostedXMinAgo = "Posted {0} min ago";
+    }
+
+    public struct S
+    {
+      public const string Start = "Start";
+      public const string Send = "Send";
+    }
+
+    public struct Y
+    {
+      public const string YourMessage = "Your message";
+    }
+
+    public struct W
+    {
+      public const string WhatIsYourName = "What is your name ?";
+      public const string WhatIsTheScanRadius = "What is the selected scan radius ?";
+    }
+  }
+}
